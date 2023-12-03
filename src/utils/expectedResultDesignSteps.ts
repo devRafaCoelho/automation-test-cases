8639db3c3d63a439e75bdd1b9b76f321e82d87e5
Rafael <devrafalcoelho@gmail.com>
2023-12-03 10:27:08 -0300

    review expected results 400

--- src/utils/expectedResultDesignSteps.ts
+++ src/utils/expectedResultDesignSteps.ts
@@ -3,6 +3,75 @@ import { getObjectsByKey } from "./helpers";
 import { createPreCondition } from "./preCondition";
 import { getResponses } from "./swagger";
 
+// export const createExpectedResultDesignSteps = async (
+//   swaggerFile: SwaggerFile
+// ): Promise<ExpectedResultDesignSteps> => {
+//   const preConditions = await createPreCondition(swaggerFile);
+//   const responses = await getResponses(swaggerFile);
+//   const expectedResultDesignSteps: ExpectedResultDesignSteps = {};
+
+//   for (const method in preConditions) {
+//     expectedResultDesignSteps[method] = [];
+
+//     await Promise.all(
+//       preConditions[method].map(async (element, index) => {
+//         const message =
+//           element.statusCode === "200"
+//             ? "Busca apresenta sucesso."
+//             : "Busca apresenta falha.";
+
+//         const expectedResultData = [
+//           `1 - ${message}`,
+//           "2 - Response retorna os seguintes valores:",
+//         ];
+
+//         const filterResponses = responses[method].filter((response) => {
+//           return response.statusCode === element.statusCode;
+//         });
+
+//         switch (element.statusCode) {
+//           case "400":
+//             const response400Required = await getResponse400Required(
+//               filterResponses
+//             );
+
+//             expectedResultData.push(
+//               JSON.stringify(response400Required, null, 2)
+//             );
+//             expectedResultData.push(`APIGEE = ${element.statusCode}`);
+
+//             expectedResultDesignSteps[method].push({
+//               statusCode: element.statusCode,
+//               value: expectedResultData,
+//             });
+//             break;
+//           default:
+//             if (filterResponses.length > 1) {
+//               expectedResultData.push(
+//                 JSON.stringify(filterResponses[index].value, null, 2)
+//               );
+//             } else {
+//               expectedResultData.push(
+//                 JSON.stringify(filterResponses[0].value, null, 2)
+//               );
+//             }
+
+//             expectedResultData.push(`APIGEE = ${element.statusCode}`);
+
+//             expectedResultDesignSteps[method].push({
+//               statusCode: element.statusCode,
+//               value: expectedResultData,
+//             });
+
+//             break;
+//         }
+//       })
+//     );
+//   }
+
+//   return expectedResultDesignSteps;
+// };
+
 export const createExpectedResultDesignSteps = async (
   swaggerFile: SwaggerFile
 ): Promise<ExpectedResultDesignSteps> => {
@@ -11,10 +80,10 @@ export const createExpectedResultDesignSteps = async (
   const expectedResultDesignSteps: ExpectedResultDesignSteps = {};
 
   for (const method in preConditions) {
-    expectedResultDesignSteps[method] = [];
+    if (preConditions.hasOwnProperty(method)) {
+      expectedResultDesignSteps[method] = [];
 
-    await Promise.all(
-      preConditions[method].map(async (element, index) => {
+      for (const [index, element] of preConditions[method].entries()) {
         const message =
           element.statusCode === "200"
             ? "Busca apresenta sucesso."
@@ -25,9 +94,9 @@ export const createExpectedResultDesignSteps = async (
           "2 - Response retorna os seguintes valores:",
         ];
 
-        const filterResponses = responses[method].filter((response) => {
-          return response.statusCode === element.statusCode;
-        });
+        const filterResponses = responses[method].filter(
+          (response) => response.statusCode === element.statusCode
+        );
 
         switch (element.statusCode) {
           case "400":
@@ -65,8 +134,8 @@ export const createExpectedResultDesignSteps = async (
 
             break;
         }
-      })
-    );
+      }
+    }
   }
 
   return expectedResultDesignSteps;
@@ -75,13 +144,46 @@ export const createExpectedResultDesignSteps = async (
 const getResponse400Required = async (filterResponses: any) => {
   let response400Required: any = {};
 
-  filterResponses.forEach((response: any) => {
-    const message = getObjectsByKey(response, "detailedMessage");
+  await Promise.all(
+    filterResponses.map(async (response: any) => {
+      if (filterResponses.length > 1) {
+        const message = getObjectsByKey(response, "detailedMessage");
+        const includeMessage =
+          message[0].includes("obrigatório") ||
+          message[0].includes("ParametroInvalido");
 
-    if (message[0].includes("obrigatório")) {
-      response400Required = response.value;
-    }
-  });
+        if (includeMessage) {
+          response400Required = response.value;
+          return;
+        }
+      } else {
+        response400Required = response.value;
+      }
+    })
+  );
 
   return response400Required;
 };
+
+// const getResponse400Required = async (
+//   filterResponses: any
+// ): Promise<ExpectedResultDesignSteps> => {
+//   let response400Required: any = {};
+
+//   for (const iterator of object) {
+
+//   }
+
+//   filterResponses.forEach((response: any) => {
+//     const message = getObjectsByKey(response, "detailedMessage");
+//     const includeMessage =
+//       message[0].includes("obrigatório") ||
+//       message[0].includes("ParametroInvalido");
+
+//     if (includeMessage) {
+//       response400Required = response.value;
+//     }
+//   });
+
+//   return response400Required;
+// };
